ee3eeec391382fd2b1036dc46528c2cd37dd7fb3
Lars Francke <lars.francke@gmail.com>
2023-11-22 00:24:05 +0100

    remove stuff

--- src/functions.rs
+++ src/functions.rs
@@ -1,4 +1,3 @@
-use std::ffi::c_char;
 use crate::{
     BulkOperation, CDataType, Char, CompletionType, ConnectionAttribute, Desc, DriverConnectOption,
     EnvironmentAttribute, FetchOrientation, FreeStmtOption, HDbc, HDesc, HEnv, HStmt, HWnd, Handle,
@@ -997,37 +996,4 @@ extern "system" {
 
 
 
-}
-
-#[cfg_attr(windows, link(name = "odbc32"))]
-#[cfg_attr(
-    all(not(windows), not(feature = "static"), not(feature = "iodbc")),
-    link(name = "odbcinst")
-)]
-#[cfg_attr(
-    all(not(windows), feature = "static", not(feature = "iodbc")),
-    link(name = "odbcinst", kind = "static")
-)]
-#[cfg_attr(
-    all(not(windows), not(feature = "static"), feature = "iodbc"),
-    link(name = "iodbcinst")
-)]
-#[cfg_attr(
-    all(not(windows), feature = "static", feature = "iodbc"),
-    link(name = "iodbcinst", kind = "static")
-)]
-extern "C" {
-    ///  Gets a list of names of values or data corresponding to a value of the system information.
-    ///
-    /// # Returns
-    ///
-    /// The amount of characters returned (negative indicates an error)
-    pub fn SQLGetPrivateProfileStringW(
-        section: *const WChar,
-        entry: *const WChar,
-        default: *const WChar,
-        ret_buffer: *mut WChar,
-        ret_buffer_size: i32,
-        filename: *const WChar,
-    ) -> i32;
 }
